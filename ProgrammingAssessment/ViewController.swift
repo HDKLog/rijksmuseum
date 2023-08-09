b3814c377ee0e23d84e24d7353f198c996dc9787
Harry S <gari_sarkisyan@epam.com>
2023-08-09 07:59:53 +0400

    Initial Commit

---
+++ ProgrammingAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammingAssessment
+//
+//  Created by Gari Sarkisyan on 09.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
