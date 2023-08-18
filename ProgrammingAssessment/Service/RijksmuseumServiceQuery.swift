5ac221eb1e5680d3c91bac641ead9fb8d57141d2
Harry S <gari_sarkisyan@epam.com>
2023-08-18 17:25:46 +0400

    add API Key

--- ProgrammingAssessment/Service/RijksmuseumServiceQuery.swift
+++ ProgrammingAssessment/Service/RijksmuseumServiceQuery.swift
@@ -39,8 +39,7 @@ class RijksmuseumServiceQuery: ServiceQuery {
     }
 
     let baseUrl = "https://www.rijksmuseum.nl/api"
-#warning("provide API KEY")
-    let key = ""
+    let key = "0fiuZFh4"
     var format: Format = .json
     var culture: Culture = .en
     var request: Request
